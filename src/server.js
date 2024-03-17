9c04b45dd0d4dde1732b7f7b595467c99d45f898
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-03-17 02:44:54 +0000

    Added  index.html as default page for server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 // Archivo: src/server.js
 
-//Se importa express
+// Se importa express
 import express from 'express';
-//Se importa usersRoute siempre se pone el .js
+import { fileURLToPath } from 'url'; // Importa fileURLToPath para convertir URL a ruta de archivo
+import path from 'path'; // Utiliza import para path
+
+// Se importa usersRoute (siempre se especifica la extensión .js)
 import usersRoute from './routes/usersRoute.js';
+
+const __filename = fileURLToPath(import.meta.url); // Obtiene la ruta del archivo actual
+const __dirname = path.dirname(__filename); // Obtiene el directorio del archivo actual
 
 const app = express();
 
 // Rutas
 app.use("/api", usersRoute);
+
+// Utiliza __dirname para establecer la ruta de la carpeta html
+app.use(express.static(path.join(__dirname, 'html')));
 
 // Servidor
 const PORT = process.env.PORT || 3000;
