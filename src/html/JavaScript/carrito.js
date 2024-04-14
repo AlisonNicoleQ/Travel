74ff79a599a17f8650589da3b49ef8c866876e85
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-04-14 22:40:38 +0000

    Carrito html

---
+++ src/html/JavaScript/carrito.js
@@ -0,0 +1,11 @@
+
+const pay = document.getElementById('pay-div');
+
+document.addEventListener("DOMContentLoaded", async() => {
+    pay.classList.toggle('hide');
+});
+
+
+function ShowPay(){
+    pay.classList.toggle('hide');
+}
